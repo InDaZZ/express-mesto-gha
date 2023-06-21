9fc41e58a74db39b77c8af0b10ce0cd70669b30f
Evgeniy Dekhtyarev <f-k-87@list.ru>
2023-06-22 01:29:30 +0300

    1 итерация

--- middleware/userValidation.js
+++ middleware/userValidation.js
@@ -1,5 +1,7 @@
 const { Joi, celebrate } = require('celebrate');
 
+const regex = /(https?:\/\/)(www)?([a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=])*#?$/;
+
 const getUserValidation = celebrate({
   params: Joi.object().keys({ userId: Joi.string().required().hex() }),
 });
@@ -13,7 +15,7 @@ const updateUserValidation = celebrate({
 
 const updateUserAvatarValidation = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().required(),
+    avatar: Joi.string().required().pattern(regex),
   }),
 });
 
@@ -21,15 +23,15 @@ const signUpValidation = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
-    email: Joi.string().required(),
+    avatar: Joi.string().pattern(regex),
+    email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 });
 
 const signInValidation = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required(),
+    email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 });
